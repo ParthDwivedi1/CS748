045751c4ab6d3f2b9016f4b63fa571d404209df7
KSatyamiitb <81472525+KSatyamiitb@users.noreply.github.com>
2023-01-12 10:01:12 +0530

    inequality left to code

--- simulator.py
+++ simulator.py
@@ -9,9 +9,22 @@ class Predictor:
         
         self.queried_state=state # updated automatically
         self.time_slots_elapsed=0 # updated automatically
+        self.time_to_tau=0 #updated automatically
+
+    def find_tau():
+        # code to find tau and update it to self.time_to_tau
+        # use cost + E[ Loss(tau to t with tau being latest queried state) ] <= E[ Loss(tau to t) ] 
+        return
+
+    def reply_for_query(self, reply):
+        self.queried_state = reply
+        self.time_slots_elapsed=0
+        self.find_tau()
+
     def predict(self):
         # Fill code here
         # To Probe, return -1
+        if self.time_slots_elapsed == self.time_to_tau: return -1
         # np.argmax(np.linalg.matrix_power(self.prob,self.time_slots_elapsed+1),axis=1)[0]
         return np.argmax(np.linalg.matrix_power(self.prob,self.time_slots_elapsed+1),axis=1)[0]
 
@@ -21,16 +34,16 @@ class Simulator:
         self.state = state
         self.horizon = horizon
         self.cost = cost
-        self.num = prob.shape[0]
+        self.num = prob.shape[0]  #number of states
     
     def next(self):
         tr=np.random.rand()
         
-        tot=0.0;
+        tot=0.0
         
         for i in range(self.num):
             if tr<=tot+self.prob[self.state][i]:
-                return i;
+                return i
             else:
                 tot+=self.prob[self.state][i]
 
@@ -38,14 +51,14 @@ class Simulator:
         loss=0.0
         
         predictor = Predictor(self.prob,self.state,self.horizon,self.cost)
+        predictor.reply_for_query(self.state) #sets up the predictor for first state provided
             
         for step in range(self.horizon):
             self.state = self.next()
-            predicted_state = predictor.predict();
+            predicted_state = predictor.predict()
             
             if(predicted_state==-1):
-                predictor.queried_state=curr_state
-                predictor.time_slots_elapsed=0
+                predictor.reply_for_query(self.state)
                 loss+=self.cost
             else:
                 predictor.time_slots_elapsed+=1
